521470fe67837803d3c8facc91dccb242e457723
Farzad <pharzath@gmail.com>
2024-02-08 13:19:34 +0330

    Add Type support agains <T>

--- BlazorTemplater/ComponentRenderer.cs
+++ BlazorTemplater/ComponentRenderer.cs
@@ -1,158 +1,153 @@
-﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Reflection;
 
-namespace BlazorTemplater
+namespace BlazorTemplater;
+
+/*
+ * Adapted from ParameterViewBuilder.cs in Egil Hansen's Genzor
+ * https://github.com/egil/genzor/blob/main/src/genzor/ParameterViewBuilder.cs
+ * Thanks for the suggestion, Egil!
+ */
+
+public class ComponentRenderer
 {
-    /*
-     * Adapted from ParameterViewBuilder.cs in Egil Hansen's Genzor
-     * https://github.com/egil/genzor/blob/main/src/genzor/ParameterViewBuilder.cs
-     * Thanks for the suggestion, Egil!
-     */
+    private const string ChildContent = nameof(ChildContent);
+    private readonly Type TComponentType;
+
+    private readonly Dictionary<string, object> parameters = new(StringComparer.Ordinal);
+    private readonly Templater templater;
+
+    #region Ctor
 
     /// <summary>
-    /// Fluent component renderer
+    /// Create a new renderer
     /// </summary>
-    /// <typeparam name="TComponent">Type of component to render</typeparam>
-    public class ComponentRenderer<TComponent> where TComponent : IComponent
+    public ComponentRenderer(Type type)
     {
-        private const string ChildContent = nameof(ChildContent);
-        private static readonly Type TComponentType = typeof(TComponent);
+        templater = new Templater();
+        TComponentType = type;
+    }
 
-        private readonly Dictionary<string, object> parameters = new(StringComparer.Ordinal);
-        private readonly Templater templater;
+    #endregion Ctor
 
-        #region Ctor
+    #region Services
 
-        /// <summary>
-        /// Create a new renderer
-        /// </summary>
-        public ComponentRenderer()
-        {
-            templater = new Templater();
-        }
+    /// <summary>
+    /// Add a service provider
+    /// </summary>
+    /// <param name="serviceProvider">a new IServiceProvider used when resolving dependencies</param>
+    /// <returns></returns>
+    public ComponentRenderer AddServiceProvider(IServiceProvider serviceProvider)
+    {
+        templater.AddServiceProvider(serviceProvider);
+        return this;
+    }
 
-        #endregion Ctor
+    /// <summary>
+    /// Fluent add-service with contract and implementation
+    /// </summary>
+    /// <typeparam name="TContract"></typeparam>
+    /// <typeparam name="TImplementation"></typeparam>
+    /// <param name="implementation"></param>
+    /// <returns></returns>
+    public ComponentRenderer AddService<TContract, TImplementation>(TImplementation implementation) where TImplementation : TContract
 
-        #region Services
+    {
+        templater.AddService<TContract, TImplementation>(implementation);
+        return this;
+    }
 
-        /// <summary>
-        /// Add a service provider
-        /// </summary>
-        /// <param name="serviceProvider">a new IServiceProvider used when resolving dependencies</param>
-        /// <returns></returns>
-        public ComponentRenderer<TComponent> AddServiceProvider(IServiceProvider serviceProvider)
-        {
-            templater.AddServiceProvider(serviceProvider);
-            return this;
-        }
+    /// <summary>
+    /// Fluent add-service with implemention
+    /// </summary>
+    /// <typeparam name="TImplementation"></typeparam>
+    /// <param name="implementation"></param>
+    /// <returns></returns>
+    public ComponentRenderer AddService<TImplementation>(TImplementation implementation)
 
-        /// <summary>
-        /// Fluent add-service with contract and implementation
-        /// </summary>
-        /// <typeparam name="TContract"></typeparam>
-        /// <typeparam name="TImplementation"></typeparam>
-        /// <param name="implementation"></param>
-        /// <returns></returns>
-        public ComponentRenderer<TComponent> AddService<TContract, TImplementation>(TImplementation implementation) where TImplementation : TContract
+    {
+        templater.AddService<TImplementation>(implementation);
+        return this;
+    }
 
-        {
-            templater.AddService<TContract, TImplementation>(implementation);
-            return this;
-        }
+    #endregion Services
 
-        /// <summary>
-        /// Fluent add-service with implemention
-        /// </summary>
-        /// <typeparam name="TImplementation"></typeparam>
-        /// <param name="implementation"></param>
-        /// <returns></returns>
-        public ComponentRenderer<TComponent> AddService<TImplementation>(TImplementation implementation)
+    #region Set Parameters
 
-        {
-            templater.AddService<TImplementation>(implementation);
-            return this;
-        }
+    /// <summary>
+    /// Sets the <paramref name="value"/> to the parameter selected with the <paramref name="parameterSelector"/>.
+    /// </summary>
+    /// <typeparam name="TValue">Type of <paramref name="value"/>.</typeparam>
+    /// <param name="parameterSelector">A lambda function that selects the parameter.</param>
+    /// <param name="value">The value to pass to <typeparamref name="TComponent"/>.</param>
+    /// <returns>This <see cref="ComponentRenderer{TComponent}"/> so that additional calls can be chained.</returns>
+    public ComponentRenderer Set<TValue>(Expression<Func<object, TValue>> parameterSelector, TValue value)
+    {
+        if (value is null)
+            throw new ArgumentNullException(nameof(value));
 
-        #endregion Services
+        parameters.Add(GetParameterName(parameterSelector), value);
+        return this;
+    }
 
-        #region Set Parameters
+    private string GetParameterName<TValue>(Expression<Func<object, TValue>> parameterSelector)
+    {
+        if (parameterSelector is null)
+            throw new ArgumentNullException(nameof(parameterSelector));
 
-        /// <summary>
-        /// Sets the <paramref name="value"/> to the parameter selected with the <paramref name="parameterSelector"/>.
-        /// </summary>
-        /// <typeparam name="TValue">Type of <paramref name="value"/>.</typeparam>
-        /// <param name="parameterSelector">A lambda function that selects the parameter.</param>
-        /// <param name="value">The value to pass to <typeparamref name="TComponent"/>.</param>
-        /// <returns>This <see cref="ComponentRenderer{TComponent}"/> so that additional calls can be chained.</returns>
-        public ComponentRenderer<TComponent> Set<TValue>(Expression<Func<TComponent, TValue>> parameterSelector, TValue value)
-        {
-            if (value is null)
-                throw new ArgumentNullException(nameof(value));
+        if (parameterSelector.Body is not MemberExpression { Member: PropertyInfo propInfoCandidate })
+            throw new ArgumentException($"The parameter selector '{parameterSelector}' does not resolve to a public property on the component '{TComponentType}'.", nameof(parameterSelector));
 
-            parameters.Add(GetParameterName(parameterSelector), value);
-            return this;
-        }
+        var propertyInfo = propInfoCandidate.DeclaringType != TComponentType
+            ? TComponentType.GetProperty(propInfoCandidate.Name, propInfoCandidate.PropertyType)
+            : propInfoCandidate;
 
-        private static string GetParameterName<TValue>(Expression<Func<TComponent, TValue>> parameterSelector)
-        {
-            if (parameterSelector is null)
-                throw new ArgumentNullException(nameof(parameterSelector));
+        var paramAttr = propertyInfo?.GetCustomAttribute<ParameterAttribute>(inherit: true);
 
-            if (parameterSelector.Body is not MemberExpression memberExpression ||
-                memberExpression.Member is not PropertyInfo propInfoCandidate)
-                throw new ArgumentException($"The parameter selector '{parameterSelector}' does not resolve to a public property on the component '{typeof(TComponent)}'.", nameof(parameterSelector));
+        if (propertyInfo is null || paramAttr is null)
+            throw new ArgumentException($"The parameter selector '{parameterSelector}' does not resolve to a public property on the component '{TComponentType}' with a [Parameter] or [CascadingParameter] attribute.", nameof(parameterSelector));
 
-            var propertyInfo = propInfoCandidate.DeclaringType != TComponentType
-                ? TComponentType.GetProperty(propInfoCandidate.Name, propInfoCandidate.PropertyType)
-                : propInfoCandidate;
+        return propertyInfo.Name;
+    }
 
-            var paramAttr = propertyInfo?.GetCustomAttribute<ParameterAttribute>(inherit: true);
+    #endregion Set Parameters
 
-            if (propertyInfo is null || paramAttr is null)
-                throw new ArgumentException($"The parameter selector '{parameterSelector}' does not resolve to a public property on the component '{typeof(TComponent)}' with a [Parameter] or [CascadingParameter] attribute.", nameof(parameterSelector));
+    #region Layout
 
-            return propertyInfo.Name;
-        }
+    /// <summary>
+    /// Set the layout to use when rendering
+    /// </summary>
+    /// <typeparam name="TLayout">The Layout type - should inherit LayoutComponentBase</typeparam>
+    /// <returns>self</returns>
+    public ComponentRenderer UseLayout<TLayout>() where TLayout : LayoutComponentBase
+    {
+        templater.UseLayout<TLayout>();
+        return this;
+    }
 
-        #endregion Set Parameters
+    /// <summary>
+    /// Set the layout to use when rendering
+    /// </summary>
+    /// <typeparam name="layoutType">The Layout type - should inherit LayoutComponentBase</typeparam>
+    /// <returns>self</returns>
+    public ComponentRenderer UseLayout(Type layoutType)
+    {
+        templater.UseLayout(layoutType);
+        return this;
+    }
 
-        #region Layout
+    #endregion Layout
 
-        /// <summary>
-        /// Set the layout to use when rendering
-        /// </summary>
-        /// <typeparam name="TLayout">The Layout type - should inherit LayoutComponentBase</typeparam>
-        /// <returns>self</returns>
-        public ComponentRenderer<TComponent> UseLayout<TLayout>() where TLayout : LayoutComponentBase
-        {
-            templater.UseLayout<TLayout>();
-            return this;
-        }
-
-        /// <summary>
-        /// Set the layout to use when rendering
-        /// </summary>
-        /// <typeparam name="layoutType">The Layout type - should inherit LayoutComponentBase</typeparam>
-        /// <returns>self</returns>
-        public ComponentRenderer<TComponent> UseLayout(Type layoutType)
-        {
-            templater.UseLayout(layoutType);
-            return this;
-        }
-
-        #endregion Layout
-
-        /// <summary>
-        /// Render the component to HTML
-        /// </summary>
-        /// <returns></returns>
-        public string Render()
-        {
-            // renders the component and returns the markup HTML
-            return templater.RenderComponent<TComponent>(parameters);
-        }
+    /// <summary>
+    /// Render the component to HTML
+    /// </summary>
+    /// <returns></returns>
+    public string Render()
+    {
+        // renders the component and returns the markup HTML
+        return templater.RenderComponent(TComponentType, parameters);
     }
 }
